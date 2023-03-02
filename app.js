2de436e3c07ea8a2096eaa50269771f36fd78928
unknown <austin.michaelwill@gmail.com>
2023-03-01 21:35:21 -0700

    More error handling

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const dotenv = require("dotenv");
 
 const connectDatabase = require("./config/database");
 const errorMiddleware = require("./middlewares/errors");
+const errorHandler = require("./utils/errorHandler");
 
 //Setting up config.env file variables
 dotenv.config({ path: "./config/config.env" });
+
+// Handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down due to uncaught exception.");
+  process.exit(1);
+});
 
 //Connecting to database
 connectDatabase();
@@ -17,8 +25,14 @@ app.use(express.json());
 
 //Import all routes
 const jobs = require("./routes/jobs");
+const ErrorHandler = require("./utils/errorHandler");
 
 app.use("/api/v1", jobs);
+
+// Handle unhandled routes
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
 
 // Middleware to handle errors
 app.use(errorMiddleware);
