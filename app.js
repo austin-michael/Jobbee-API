d5d72c7c9569fe76ad897667615e4c52b1e1aab9
unknown <austin.michaelwill@gmail.com>
2023-03-15 17:43:11 -0600

    Added cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xssClean = require("xss-clean");
 const hpp = require("hpp");
+const cors = require("cors");
 
 const connectDatabase = require("./config/database");
 const errorMiddleware = require("./middlewares/errors");
@@ -53,6 +54,9 @@ const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100,
 });
+
+// Setup CORS - Accessible by other domains
+app.use(cors());
 
 app.use(limiter);
 
